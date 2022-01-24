7631791dce98c11b8e4f96b681348affa5fa4ae2
Himani <himani9.engg@gmail.com>
2022-01-24 21:11:16 +0530

    taken from resource

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
-const selectSong = (song) => {
+// Action creator
+export const selectSong = song => {
+  // Return an action
   return {
-    type: "SONG_SELECTED",
-    payload: song,
+    type: 'SONG_SELECTED',
+    payload: song
   };
 };
-
-// export default selectSong;
